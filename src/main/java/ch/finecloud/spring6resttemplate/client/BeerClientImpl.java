065607b10e7f945c26341ccd1badaf447c2980a5
zisuu <david.ziswiler@finecloud.ch>
2023-04-23 19:34:15 +0200

    get beer list as java map

--- src/main/java/ch/finecloud/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/ch/finecloud/spring6resttemplate/client/BeerClientImpl.java
@@ -8,17 +8,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beers";
 
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<String> stringResponse = restTemplate.getForEntity("http://localhost:8080/api/v1/beers", String.class);
+        ResponseEntity<String> stringResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
 
         System.out.println(stringResponse.getBody());
         return null;
